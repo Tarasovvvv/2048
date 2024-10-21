e658f912483df58cbeac6353b65a29310127a571
Tarasovvvv <steelcoat@mail.com>
2024-10-21 23:53:03 +0400

    put components into the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Game, History, Mode, Theme } from './components/index';
+
 function App() {
 
   return (
     <>
+      <Game></Game>
+      <History></History>
+      <Mode></Mode>
+      <Theme></Theme>
     </>
   )
 }
